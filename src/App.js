00a1e1a8c795bb02e51223de87e87d5883a90e98
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-28 14:03:36 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Test</h1>}>
+            <Route index element={<h1>sss</h1>} />
+            <Route path="about" element={<h1>Test</h1>} />
+            <Route path="dashboard" element={<h1>Test</h1>} />
+
+            {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+            <Route path="*" element={<h1>Test</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
