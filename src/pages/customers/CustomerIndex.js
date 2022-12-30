6ad3ec2565041400e71044a07ca91afc7d65d7d2
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-30 16:45:11 +0700

    Add create button

--- src/pages/customers/CustomerIndex.js
+++ src/pages/customers/CustomerIndex.js
@@ -1,8 +1,10 @@
-import { Card } from "@mui/material";
+import { Button, Card } from "@mui/material";
 import SmartTable from "../../components/SmartTable";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchCustomers } from "../../slices/customerSlice";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -23,16 +25,23 @@ export default function CustomerIndex() {
   }, [dispatch]);
 
   return (
-    <Card>
-      <SmartTable
-        rows={customers.map((customer) => ({
-          id: customer.id,
-          fullName: customer.fullName,
-          phone: customer.phone,
-          address: customer.address,
-        }))}
-        columns={columns}
-      />
-    </Card>
+    <Box>
+      <Box paddingBottom={2}>
+        <Button variant="contained" component={Link} to={"/customers/create"}>
+          New Customer
+        </Button>
+      </Box>
+      <Card>
+        <SmartTable
+          rows={customers.map((customer) => ({
+            id: customer.id,
+            fullName: customer.fullName,
+            phone: customer.phone,
+            address: customer.address,
+          }))}
+          columns={columns}
+        />
+      </Card>
+    </Box>
   );
 }
