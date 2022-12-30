ef291051a9b7ec4ffea7ed4cf324a8a556ea1864
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-30 13:34:22 +0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import Layout from "./components/Layout";
+import CustomerIndex from "./pages/customers/CustomerIndex";
+import DrIdItemIndex from "./pages/drSecret/id/itemIndex";
 
 function App() {
   return (
@@ -10,12 +12,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<h1>Dashboard</h1>} />
+            <Route path="customers" element={<CustomerIndex />} />
             <Route path="other" element={<h1>Other Page</h1>} />
-
-            {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
             <Route path="*" element={<h1>404 Not Found</h1>} />
+            <Route path="dr-secret">
+              <Route path="id">
+                <Route path="items" element={<DrIdItemIndex />} />
+              </Route>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
