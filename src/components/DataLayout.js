14ccd4b0946af3078721cf0b1944e04488399e25
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-12-30 16:44:49 +0700

    Switch from h1 to typography from MUI

--- src/components/DataLayout.js
+++ src/components/DataLayout.js
@@ -1,9 +1,12 @@
+import { Typography } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 export default function DataLayout({ title }) {
   return (
     <main>
-      <h1>{title}</h1>
+      <Typography variant="h3" marginY={2} component="h1">
+        {title}
+      </Typography>
       <Outlet />
     </main>
   );
