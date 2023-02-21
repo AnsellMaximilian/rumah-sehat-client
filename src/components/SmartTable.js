728c4f750b05962a0caf0cbd7fc016e029ed5e25
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-02-21 13:26:29 +0700

    Update smart table

--- src/components/SmartTable.js
+++ src/components/SmartTable.js
@@ -1,14 +1,18 @@
 import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
 
 export default function SmartTable({ rows, columns }) {
+  const [pageSize, setPageSize] = useState(5);
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={pageSize}
+        rowsPerPageOptions={[5, 10, 20]}
+        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
         checkboxSelection
+        rowHeight={25}
       />
     </div>
   );
