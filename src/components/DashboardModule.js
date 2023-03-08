c606c35aa57b9d6e402842267e1aeaa694c90742
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-03-08 15:35:13 +0700

    Add change for adding custom link

--- src/components/DashboardModule.js
+++ src/components/DashboardModule.js
@@ -1,17 +1,30 @@
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
 import Table from "./Table";
 
-export default function DashboardModule({ columns, rows, title }) {
+export default function DashboardModule({
+  columns,
+  rows,
+  title,
+  linkText = "See More",
+  linkTo = "/",
+}) {
   return (
     <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
       <Typography component="h2" variant="h6" color="primary" gutterBottom>
         {title}
       </Typography>
       <Table rows={rows} columns={columns} size="small" />
-      <Link color="primary" href="#" sx={{ mt: 3 }}>
-        See more orders
+      <Link
+        color="primary"
+        sx={{ mt: 3, textDecoration: "none" }}
+        component={RouterLink}
+        to={linkTo}
+        underline="hover"
+      >
+        {linkText}
       </Link>
     </Paper>
   );
