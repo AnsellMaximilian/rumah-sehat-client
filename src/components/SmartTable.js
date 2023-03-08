a5179efc593f727de79f5afd41b55df753f1969a
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-03-08 09:40:19 +0700

    Adjust default smart table page size

--- src/components/SmartTable.js
+++ src/components/SmartTable.js
@@ -2,14 +2,14 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useState } from "react";
 
 export default function SmartTable({ rows, columns }) {
-  const [pageSize, setPageSize] = useState(5);
+  const [pageSize, setPageSize] = useState(10);
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={pageSize}
-        rowsPerPageOptions={[5, 10, 20]}
+        rowsPerPageOptions={[10, 20, 30]}
         onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
         checkboxSelection
         rowHeight={25}
