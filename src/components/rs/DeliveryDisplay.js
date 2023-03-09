c5298eff32ed5ad00a2d30dcb76675a3b03a95d7
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-03-09 17:02:05 +0700

    Add delivery display component

---
+++ src/components/rs/DeliveryDisplay.js
@@ -0,0 +1,141 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Collapse from "@mui/material/Collapse";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+// import moment from "moment";
+import Table from "../Table";
+import { getSubtotal, getTableColumn } from "../../helpers/rs";
+import NumericFormatRp from "../NumericFormatRp";
+import { useState } from "react";
+
+export default function DeliveryDisplay({ delivery }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Paper sx={{ paddingBottom: 2 }}>
+      <Box xs={12} display="flex" alignItems="flex-start" paddingLeft={2}>
+        <Box paddingY={2}>
+          <Typography variant="subtitle1" fontWeight="bold">
+            #{delivery.id}
+          </Typography>
+        </Box>
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ marginLeft: "auto" }}
+          // onClick={() => handleRemoveDelivery(delivery.key)}
+        >
+          <CloseIcon />
+        </Button>
+      </Box>
+      <Grid container spacing={1} sx={{ padding: 2 }}>
+        <Grid item xs={6}>
+          <Typography variant="subtitle2">Date</Typography>
+          <Typography>{delivery.date}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle2">Recipient</Typography>
+          <Typography>{delivery.customerFullName}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle2">Note</Typography>
+          <Typography>{delivery.note || "None"}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle2">Delivery Type</Typography>
+          <Typography>{delivery.DeliveryType.name}</Typography>
+        </Grid>
+      </Grid>
+      <Box display="flex" gap={2} marginRight={1}>
+        <Box
+          sx={{ backgroundColor: "primary.main", color: "white", flex: 1 }}
+          display="flex"
+          alignItems="center"
+          paddingLeft={2}
+        >
+          <Typography>Details</Typography>
+        </Box>
+        <Button variant="outlined" onClick={() => setIsOpen(!isOpen)}>
+          Collapse
+        </Button>
+      </Box>
+      <Collapse in={isOpen}>
+        <Table
+          size="small"
+          rows={delivery.DeliveryDetails}
+          columns={[
+            getTableColumn("Product", "productName"),
+            getTableColumn("Cost", "cost", (row) => (
+              <NumericFormatRp value={parseFloat(row.cost)} />
+            )),
+            getTableColumn("Price", "price", (row) => (
+              <NumericFormatRp value={parseFloat(row.price)} />
+            )),
+            getTableColumn("Qty", "qty", (row) => parseFloat(row.qty)),
+            getTableColumn(
+              "Subtotal",
+              "totalPrice",
+              (row) => <NumericFormatRp value={parseFloat(row.totalPrice)} />,
+              { align: "right" }
+            ),
+          ]}
+          tableBottom={
+            <>
+              <TableRow>
+                <TableCell
+                  colSpan={4}
+                  align="right"
+                  component="th"
+                  sx={{ fontWeight: "500" }}
+                >
+                  Subtotal
+                </TableCell>
+                <TableCell align="right">
+                  <NumericFormatRp
+                    value={parseInt(getSubtotal(delivery.DeliveryDetails))}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  colSpan={4}
+                  align="right"
+                  component="th"
+                  sx={{ fontWeight: "500" }}
+                >
+                  Delivery Cost
+                </TableCell>
+                <TableCell align="right">
+                  <NumericFormatRp value={parseInt(delivery.cost)} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  colSpan={4}
+                  align="right"
+                  component="th"
+                  sx={{ fontWeight: "500" }}
+                >
+                  Total
+                </TableCell>
+                <TableCell align="right">
+                  <NumericFormatRp
+                    value={parseInt(
+                      getSubtotal(delivery.DeliveryDetails) +
+                        parseInt(delivery.cost)
+                    )}
+                  />
+                </TableCell>
+              </TableRow>
+            </>
+          }
+        />
+      </Collapse>
+    </Paper>
+  );
+}
