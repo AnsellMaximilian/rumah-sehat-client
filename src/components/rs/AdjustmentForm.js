b0826e318e64c66abb6d76fb55b2105057127d4b
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-03-21 11:23:23 +0700

    Add adjustments

---
+++ src/components/rs/AdjustmentForm.js
@@ -0,0 +1,184 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Paper from "@mui/material/Paper";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import TableHead from "@mui/material/TableHead";
+import TableCell from "@mui/material/TableCell";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Autocomplete from "@mui/material/Autocomplete";
+import Tooltip from "@mui/material/Tooltip";
+import { toast } from "react-toastify";
+
+import Cancel from "@mui/icons-material/Cancel";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import http from "../../http-common";
+import AutoSelectTextField from "../AutoSelectTextField";
+import NumericFormatRp from "../NumericFormatRp";
+
+export default function AdjustmentForm({
+  onSubmit,
+  onCancel,
+  CustomerId,
+  editId,
+}) {
+  const [amount, setAmount] = useState(0);
+  const [selectedSourceInvoice, setSelectedSourceInvoice] = useState(null);
+  const [selectedAdjustedInvoice, setSelectedAdjustedInvoice] = useState(null);
+  const [description, setDescription] = useState("");
+  const [invoices, setInvoices] = useState([]);
+
+  useState(() => {
+    (async () => {
+      if (CustomerId) {
+        setInvoices(
+          (await http.get(`/rs/invoices?CustomerId=${CustomerId}`)).data.data
+        );
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (editId) {
+        const adjustment = (await http.get(`/rs/adjustments/${editId}`)).data
+          .data;
+        setDescription(adjustment.description);
+        setAmount(adjustment.amount);
+        setSelectedSourceInvoice(adjustment.SourceInvoice);
+        setSelectedAdjustedInvoice(adjustment.AdjustedInvoice);
+      }
+    })();
+  }, [editId]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (
+        selectedSourceInvoice &&
+        selectedAdjustedInvoice &&
+        selectedSourceInvoice.id === selectedAdjustedInvoice.id
+      )
+        throw new Error("Source invoice and adjusted invoice are the same.");
+      const body = {
+        description,
+        amount,
+        SourceInvoiceId: selectedSourceInvoice
+          ? selectedSourceInvoice.id
+          : null,
+        AdjustedInvoiceId: selectedAdjustedInvoice
+          ? selectedAdjustedInvoice.id
+          : null,
+        CustomerId,
+      };
+      if (!editId) {
+        await http.post("/rs/adjustments", body);
+        toast.success("Created adjustment.");
+      } else {
+        await http.patch(`/rs/adjustments/${editId}`, body);
+        toast.success("Updated adjustment.");
+      }
+      onSubmit();
+    } catch (error) {
+      const errorValue = error?.response?.data || error?.response?.data.error;
+      const errorMsg = errorValue ? errorValue : error.message;
+      toast.error(errorMsg);
+    }
+  };
+
+  return invoices.length > 0 ? (
+    <Box component={Paper} padding={2}>
+      <Typography variant="h5">
+        {editId ? "Editing" : "Create"} Adjustment
+      </Typography>
+      <Grid container spacing={2} marginTop={2}>
+        <Grid item xs={4}>
+          <Autocomplete
+            value={selectedSourceInvoice}
+            onChange={(e, newValue) => {
+              setSelectedSourceInvoice(newValue);
+            }}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderOption={(props, option) => (
+              <li {...props} key={option.id}>
+                {option.id}
+              </li>
+            )}
+            getOptionLabel={(option) =>
+              `(#${option.id}) ${option.customerFullName} - ${option.status}`
+            }
+            options={invoices}
+            renderInput={(params) => (
+              <TextField {...params} fullWidth label="Source Invoice" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <AutoSelectTextField
+            label="Amount"
+            margin="none"
+            type="number"
+            fullWidth
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Autocomplete
+            value={selectedAdjustedInvoice}
+            onChange={(e, newValue) => {
+              setSelectedAdjustedInvoice(newValue);
+            }}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderOption={(props, option) => (
+              <li {...props} key={option.id}>
+                {option.id}
+              </li>
+            )}
+            getOptionLabel={(option) =>
+              `(#${option.id}) ${option.customerFullName} - ${option.status}`
+            }
+            options={invoices}
+            renderInput={(params) => (
+              <TextField {...params} fullWidth label="Adjusted Invoice" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            multiline
+            fullWidth
+            margin="none"
+            label="Description"
+            rows={3.45}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button fullWidth variant="outlined" onClick={onCancel}>
+            Cancel
+          </Button>
+        </Grid>
+        <Grid item xs={8}>
+          <Button fullWidth variant="contained" onClick={handleSubmit}>
+            {editId ? "Update" : "Create"}
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  ) : (
+    <h1>Loading...</h1>
+  );
+}
--- src/pages/customers/CustomerShow.js
+++ src/pages/customers/CustomerShow.js
@@ -1,27 +1,66 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
+import Dialog from "@mui/material/Dialog";
+import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import moment from "moment";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NumericFormatRp from "../../components/NumericFormatRp";
 import http from "../../http-common";
 import PrintIcon from "@mui/icons-material/Print";
+
 import { Link } from "react-router-dom";
 import ValueDisplay from "../../components/ValueDisplay";
+import AdjustmentForm from "../../components/rs/AdjustmentForm";
+import Table from "../../components/Table";
+import { getTableColumn } from "../../helpers/rs";
+import Delete from "@mui/icons-material/Delete";
+import Edit from "@mui/icons-material/ModeEdit";
+import DeleteAlert from "../../components/DeleteAlert";
+import { toast } from "react-toastify";
 
 export default function CustomerShow() {
   const { id } = useParams();
   const [customer, setCustomer] = useState(null);
+
+  // Adjustments
+  const [adjustmentFormOpen, setAdjustmentFormOpen] = useState(false);
+
+  //edit
+  const [editId, setEditId] = useState(null);
+
+  //delete
+  const [toDeleteId, setToDeleteId] = useState(null);
+
+  const handleAdjustmentFormClose = () => setAdjustmentFormOpen(false);
+  const handleAdjustmentSubmit = async () => {
+    handleAdjustmentFormClose();
+    setCustomer((await http.get(`/customers/${id}`)).data.data);
+
+    setEditId(null);
+  };
+
+  const handleEdit = (id) => {
+    setEditId(id);
+    setAdjustmentFormOpen(true);
+  };
+
+  const handleDelete = (id) => {
+    (async () => {
+      try {
+        const adjustment = (await http.delete(`/rs/adjustments/${id}`)).data
+          .data;
+        setCustomer((await http.get(`/customers/${customer.id}`)).data.data);
+
+        toast.success(`Adjustment #${adjustment.id} deleted.`);
+      } catch ({ response: { data: error } }) {
+        toast.error(error);
+      }
+    })();
+  };
 
   useEffect(() => {
     (async () => {
@@ -29,32 +68,102 @@ export default function CustomerShow() {
     })();
   }, [id]);
   return customer ? (
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <Paper sx={{ padding: 2, height: "100%" }}>
-          <Box display="flex" flexDirection="column">
-            <Typography variant="subtitle" fontWeight="bold" color="GrayText">
-              #{customer.id}
-            </Typography>
-            <Typography variant="h5" fontWeight="bold" lineHeight={0.7}>
-              {customer.fullName}
-            </Typography>
-          </Box>
-          <Box display="flex" flexWrap="wrap" gap={2} marginTop={2}>
-            <ValueDisplay value={customer.phone} label="Phone" />
-            <ValueDisplay value={customer.Region.name} label="Region" />
-            <ValueDisplay value={customer.address} label="Address" />
-          </Box>
-        </Paper>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Paper sx={{ padding: 2, height: "100%" }}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="subtitle" fontWeight="bold" color="GrayText">
+                #{customer.id}
+              </Typography>
+              <Typography variant="h5" fontWeight="bold" lineHeight={0.7}>
+                {customer.fullName}
+              </Typography>
+            </Box>
+            <Box display="flex" flexWrap="wrap" gap={2} marginTop={2}>
+              <ValueDisplay value={customer.phone} label="Phone" />
+              <ValueDisplay value={customer.Region.name} label="Region" />
+              <ValueDisplay value={customer.address} label="Address" />
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper sx={{ padding: 2, height: "100%" }}>
+            <Box display="flex" justifyContent="space-between" gap={2}>
+              <Typography variant="h6" fontWeight="bold">
+                Adjustments
+              </Typography>
+
+              <Button
+                variant="contained"
+                onClick={() => setAdjustmentFormOpen(true)}
+              >
+                Create
+              </Button>
+            </Box>
+            <Table
+              sx={{ marginTop: 2 }}
+              size="small"
+              rows={customer.Adjustments.map((adjustment) => ({
+                id: adjustment.id,
+                SourceInvoiceId: adjustment.SourceInvoiceId,
+                amount: adjustment.amount,
+                description: adjustment.description,
+                AdjustedInvoiceId: adjustment.AdjustedInvoiceId,
+              }))}
+              columns={[
+                getTableColumn("ID", "id"),
+                getTableColumn("Source", "SourceInvoiceId"),
+                getTableColumn("Amount", "amount"),
+                getTableColumn("Description", "description"),
+                getTableColumn("Adjusted", "AdjustedInvoiceId"),
+                getTableColumn("Actions", "actions", (row) => (
+                  <>
+                    <IconButton
+                      color="warning"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleEdit(row.id);
+                      }}
+                    >
+                      <Edit />
+                    </IconButton>
+                    <IconButton
+                      color="error"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setToDeleteId(row.id);
+                      }}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </>
+                )),
+              ]}
+            />
+            <Dialog
+              open={adjustmentFormOpen}
+              onClose={handleAdjustmentFormClose}
+              maxWidth="xl"
+            >
+              <AdjustmentForm
+                CustomerId={customer.id}
+                onSubmit={handleAdjustmentSubmit}
+                onCancel={handleAdjustmentFormClose}
+                editId={editId}
+              />
+            </Dialog>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Paper sx={{ padding: 2, height: "100%" }}>
-          <Typography variant="h6" fontWeight="bold">
-            Adjustments
-          </Typography>
-        </Paper>
-      </Grid>
-    </Grid>
+      <DeleteAlert
+        message={`Are you sure you want to delete adjustment #${toDeleteId}.`}
+        toDeleteId={toDeleteId}
+        handleDelete={handleDelete}
+        setToDeleteId={setToDeleteId}
+        objectName="Adjustment"
+      />
+    </>
   ) : (
     <h1>Loading...</h1>
   );
