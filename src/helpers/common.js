ac59d2de5eaaafe3ca197f5ed5b45c2a3135d385
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-09 13:59:00 +0700

    Fix set week function

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -3,16 +3,8 @@ import moment from "moment";
 
 export const getWeek = () => {
   const currentDate = moment();
-  const weekStart = currentDate
-    .clone()
-    .startOf("week")
-    .add(1, "day")
-    .format("yyyy-MM-DD");
-  const weekEnd = currentDate
-    .clone()
-    .endOf("week")
-    .add(1, "day")
-    .format("yyyy-MM-DD");
+  const weekStart = currentDate.clone().startOf("isoWeek").format("yyyy-MM-DD");
+  const weekEnd = currentDate.clone().endOf("isoWeek").format("yyyy-MM-DD");
 
   return { weekStart, weekEnd };
 };
